427d01819db9ede16245bb24f6c00e28f9b01ed6
黒羽あおは <kurohaaoha@kurohaaohanoMacBook-Pro.local>
2021-12-11 11:58:54 +0900

    header,footer,sidebarをパーツファイル化

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="l-footer">
+    <ul>
+        <li>ショップ情報　|　ヒストリー</li>
+    </ul>
+    <address>Copyright: RaiseTech</address>
+</footer>
+<?php wp_footer(); ?>
---
+++ header.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<title><?php the_title(); ?></title>
+<meta name="description" content="Hamburger">
+<meta name="viewport" contetn="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+
+<!-- CSS -->
+<link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/public/css/style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;400&display=swap" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
+<?php wp_head(); ?>
