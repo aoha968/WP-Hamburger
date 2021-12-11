d932bc15eec43db75f99083596e999fa625cd031
黒羽あおは <kurohaaoha@kurohaaohanoMacBook-Pro.local>
2021-12-11 12:06:50 +0900

    プロトコルURL対策とJSのパスを変更

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
 
 <!-- CSS -->
-<link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css">
+<link rel="stylesheet" href="//unpkg.com/modern-css-reset/dist/reset.min.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/public/css/style.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;400&display=swap" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript" src="./js/main.js"></script>
+<link rel="preconnect" href="//fonts.googleapis.com">
+<link rel="preconnect" href="//fonts.gstatic.com" crossorigin>
+<link href="//fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;400&display=swap" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 <?php wp_head(); ?>
