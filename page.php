c8124982c30a219646bcdb44775f4f7bb2a4293f
黒羽あおは <kurohaaoha@kurohaaohanoMacBook-Pro.local>
2021-12-16 02:06:02 +0900

    page.phpのheaderをパーシャルファイルから読み込むように変更

--- page.php
+++ page.php
@@ -7,16 +7,7 @@
     <body>
         <main class="l-main">
             <div class="l-main__container">
-                <header class="l-header">
-                    <button class="l-header__menu-btn">Menu</button>
-                    <a href="#"><h1 class="l-header__logo">Hamburger</h1></a>
-                    <form class="l-header__form">
-                        <div class="l-header__form__wrap">
-                            <input type="text" class="l-header__form__search">
-                        </div>
-                        <button type="submit" name="" class="l-header__form__btn" value="">検索</button>
-                    </form>
-                </header>
+                <?php get_template_part('includes/header'); ?>
 
                 <article class="l-main__content">
                     <div class="l-visual -page">
