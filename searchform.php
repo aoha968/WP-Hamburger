34aae6c5c4252f352223144160f1ca9936a052ba
黒羽あおは <kurohaaoha@kurohaaohanoMacBook-Pro.local>
2021-12-20 22:09:47 +0900

    search formをコンポーネント化

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form class="l-header__form" method="get" action="<?php echo esc_url(home_url()); ?>">
     <div class="l-header__form__wrap">
-        <input type="text" class="l-header__form__search" name="s" id="s" placeholder="キーワードで検索する">
+        <input type="text" class="search-form" name="s" id="s" placeholder="キーワードで検索する">
     </div>
     <button type="submit" name="" class="c-submit-btn" value="">検索</button>
 </form>
